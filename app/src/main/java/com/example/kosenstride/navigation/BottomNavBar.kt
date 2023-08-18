ff799b5ed186fe8c5769f4d60e1e481ad8dd10a1
kototo522 <huyejiben@gmail.com>
2023-08-18 22:58:23 +0900

    add: navigationBarを下部に固定

--- app/src/main/java/com/example/kosenstride/navigation/BottomNavBar.kt
+++ app/src/main/java/com/example/kosenstride/navigation/BottomNavBar.kt
@@ -1,6 +1,8 @@
 package com.example.kosenstride.navigation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.List
@@ -13,6 +15,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.compose.NavHost
@@ -41,41 +44,43 @@ sealed class BottomBarScreen(
             BottomBarScreen.ToDoList,
             BottomBarScreen.Setting
         )
-
         val navStackBackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navStackBackEntry?.destination?.route
 
         Surface {
-            NavHost(
-                navController = navController,
-                startDestination = BottomBarScreen.Home.route
-            ) {
-                composable(route = BottomBarScreen.Home.route) {
-                    HomeScreen()
+            Box(modifier = Modifier.fillMaxSize()) {
+                NavHost(
+                    navController = navController,
+                    startDestination = BottomBarScreen.Home.route
+                ) {
+                    composable(route = BottomBarScreen.Home.route) {
+                        HomeScreen()
+                    }
+                    composable(route = BottomBarScreen.ToDoList.route) {
+                        ToDoListScreen()
+                    }
+                    composable(route = BottomBarScreen.Setting.route) {
+                        SettingScreen()
+                    }
                 }
-                composable(route = BottomBarScreen.ToDoList.route) {
-                    ToDoListScreen()
-                }
-                composable(route = BottomBarScreen.Setting.route) {
-                    SettingScreen()
-                }
-            }
-            NavigationBar(
-                modifier = Modifier.background(MaterialTheme.colorScheme.background),
-                contentColor = MaterialTheme.colorScheme.primary
-            ) {
-                screenItems.forEach { screen ->
-                    NavigationBarItem(
-                        icon = { Icon(imageVector = screen.icon, contentDescription = null) },
-                        label = { Text(text = screen.title) },
-                        selected = currentRoute == screen.route,
-                        onClick = {
-                            navController.navigate(screen.route) {
-                                popUpTo(navController.graph.startDestinationId)
-                                launchSingleTop = true
+                NavigationBar(
+                    modifier = Modifier.align(Alignment.BottomCenter)
+                        .background(MaterialTheme.colorScheme.background),
+                    contentColor = MaterialTheme.colorScheme.primary
+                ) {
+                    screenItems.forEach { screen ->
+                        NavigationBarItem(
+                            icon = { Icon(imageVector = screen.icon, contentDescription = null) },
+                            label = { Text(text = screen.title) },
+                            selected = currentRoute == screen.route,
+                            onClick = {
+                                navController.navigate(screen.route) {
+                                    popUpTo(navController.graph.startDestinationId)
+                                    launchSingleTop = true
+                                }
                             }
-                        }
-                    )
+                        )
+                    }
                 }
             }
         }

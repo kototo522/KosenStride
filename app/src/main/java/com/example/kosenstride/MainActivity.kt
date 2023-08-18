f2f84cfdaee31089cea275fabce2da5c685cdb08
kototo522 <huyejiben@gmail.com>
2023-08-18 10:19:55 +0900

    add: BottomNavBarの表示

--- app/src/main/java/com/example/kosenstride/MainActivity.kt
+++ app/src/main/java/com/example/kosenstride/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.kosenstride
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.kosenstride.navigation.BottomNavigationBar
 import com.example.kosenstride.ui.theme.KosenStrideTheme
 
@@ -28,21 +24,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KosenStrideTheme {
-        Greeting("Android")
     }
 }

a4a04ec5d8f25b99ce91f6e20b71939daae8106f
kototo522 <huyejiben@gmail.com>
2023-08-20 11:25:48 +0900

    add: ListCardのUI作成

--- app/src/main/java/com/example/kosenstride/ui/todo/TodoListScreen.kt
+++ app/src/main/java/com/example/kosenstride/ui/todo/TodoListScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.kosenstride.ui.todo.component.ListCard
 
 @Composable
 fun ToDoListScreen(){
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Text(text = "ToDoList Screen", style = MaterialTheme.typography.bodyMedium)
+        ListCard()
     }
 }
---
+++ app/src/main/java/com/example/kosenstride/ui/todo/component/ListCard.kt
@@ -0,0 +1,96 @@
+package com.example.kosenstride.ui.todo.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ListCard(modifier: Modifier = Modifier) {
+    Card(
+        shape = RoundedCornerShape(0.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background,
+        ),
+    ) {
+        Column {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "sample title",
+                    modifier = Modifier
+                        .padding(
+                            horizontal = 24.dp, vertical = 8.dp
+                        )
+                        .weight(5f),
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(24.dp)
+                        .weight(1f),
+                ) {
+                }
+                Spacer(modifier = modifier.weight(1f))
+                Text(
+                    text = "2023/8/20/17:00",
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .weight(2f),
+                    style = MaterialTheme.typography.bodySmall,
+                )
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Notifications,
+                        contentDescription = "通知",
+                        modifier = modifier
+                            .weight(1f)
+                            .width(20.dp),
+                    )
+                }
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Send,
+                        contentDescription = "共有",
+                        modifier = modifier
+                            .weight(1f)
+                            .width(20.dp),
+                    )
+                }
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(
+                        imageVector = Icons.Filled.MoreVert,
+                        contentDescription = "その他",
+                        modifier = modifier
+                            .weight(1f)
+                            .width(20.dp),
+                    )
+                }
+            }
+            Divider(color = Color.Gray)
+        }
+    }
+}

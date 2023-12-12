dfb31d7edd50858a3ecbea349280e129c8baba81
kototo522 <huyejiben@gmail.com>
2023-12-12 14:50:05 +0900

    add: topAppBarの追加

--- app/src/main/java/com/example/kosenstride/MainActivity.kt
+++ app/src/main/java/com/example/kosenstride/MainActivity.kt
@@ -3,24 +3,28 @@ package com.example.kosenstride
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.kosenstride.navigation.BottomNavigationBar
+import com.example.kosenstride.topAppBar.KosenStrideTopAppBar
 import com.example.kosenstride.ui.theme.KosenStrideTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KosenStrideTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxWidth(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
-                    BottomNavigationBar()
+                    Column {
+                        KosenStrideTopAppBar()
+                        BottomNavigationBar()
+                    }
                 }
             }
         }

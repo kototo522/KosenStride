999868ff6a3e66bdef737f5a50c2345deee54e56
kototo522 <huyejiben@gmail.com>
2023-12-12 14:04:04 +0900

    chenge: ListCardのUIの変更

--- app/src/main/java/com/example/kosenstride/ui/todo/TodoListScreen.kt
+++ app/src/main/java/com/example/kosenstride/ui/todo/TodoListScreen.kt
@@ -1,28 +1,53 @@
 package com.example.kosenstride.ui.todo
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kosenstride.ui.todo.component.ListCard
 
 data class CardItem(
     val id: String,
     val title: String?,
+    val text: String?,
+    val dateTime: String,
     val notifications: Boolean,
     val share: Boolean,
 )
 
 val CardItemList = listOf(
-    CardItem("Task 1", "数値計算 WebClass", false,true),
-    CardItem("Task 2", "電気回路 プリント 課題８", true, false),
-    CardItem("Task 3", "電気回路 プリント 課題９", false, false),
+    CardItem("Task 1", "数値計算 WebClass", "問題１", "2023/8/20/17:00",
+        notifications = false,
+        share = true
+    ),
+    CardItem("Task 2", "数値計算 WebClass", "問題2", "2023/8/20/18:00",
+        notifications = false,
+        share = true
+    ),
+    CardItem("Task 3", "数値計算 WebClass", "問題3", "2023/8/20/19:00",
+        notifications = false,
+        share = true
+    ),
 )
 @Composable
 fun ToDoListScreen(){
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(12.dp, Alignment.Top),
+        horizontalAlignment = Alignment.Start,
+        ) {
         itemsIndexed(CardItemList) { index, CardItem ->
             ListCard(index, CardItemList[index], Modifier)
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewTodoListScreen() {
+    ToDoListScreen()
 }
--- app/src/main/java/com/example/kosenstride/ui/todo/component/ListCard.kt
+++ app/src/main/java/com/example/kosenstride/ui/todo/component/ListCard.kt
@@ -1,9 +1,8 @@
 package com.example.kosenstride.ui.todo.component
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,10 +11,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -24,7 +22,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kosenstride.ui.todo.CardItem
 
 @Composable
@@ -35,57 +38,73 @@ fun ListCard(index: Int, cardItem: CardItem, modifier: Modifier = Modifier) {
     val shareButtonColor =
         if(cardItem.share){Color.Blue}
         else{Color.DarkGray}
+
     Card(
-        shape = RoundedCornerShape(0.dp),
+        shape = RoundedCornerShape(size = 5.dp),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.background,
         ),
+        modifier = Modifier
+            .padding(start = 8.dp, top = 4.dp, end = 8.dp)
+            .border(width = 1.dp, color = Color(0xFF215FA6), shape = RoundedCornerShape(size = 5.dp))
+            .fillMaxWidth()
     ) {
-        Column {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = "${index+1}. ${cardItem.title}",
-                    modifier = Modifier
-                        .padding(
-                            horizontal = 12.dp, vertical = 8.dp
-                        )
-                        .weight(5f),
-                    style = MaterialTheme.typography.titleLarge,
+        Column{
+            Text(
+                text = "${index+1}. ${cardItem.title}",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    lineHeight = 16.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF3D4758),
+                    letterSpacing = 0.5.sp,
+                ),
+                modifier = Modifier.padding(4.dp)
+            )
+            Text(
+                text = "${cardItem.text}",
+                modifier = Modifier.padding(start = 20.dp, top = 4.dp),
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    lineHeight = 14.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF3D4758),
+                    letterSpacing = 0.5.sp,
                 )
-            }
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(18.dp)
-                        .weight(1f),
-                ) {
-                }
-                Spacer(modifier = modifier.weight(1f))
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.End)
+                    .height(40.dp)
+            ) {
                 Text(
-                    text = "2023/8/20/17:00",
+                    text = cardItem.dateTime,
                     modifier = Modifier
-                        .padding(8.dp)
-                        .weight(2f),
-                    style = MaterialTheme.typography.bodySmall,
+                        .weight(4f)
+                        .padding(10.dp),
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 14.sp,
+                        fontWeight = FontWeight(700),
+                        color = Color(0xFF3D4758),
+                        textAlign = TextAlign.Right,
+                        letterSpacing = 0.5.sp,
+                    )
                 )
                 IconButton(onClick = { /* TODO */ }) {
                     Icon(
                         imageVector = Icons.Filled.Notifications,
                         contentDescription = "通知",
-                        modifier = modifier
-                            .weight(1f)
-                            .width(20.dp),
+                        modifier = Modifier.width(20.dp),
                         tint = notificationColor
                     )
                 }
                 IconButton(onClick = { /* TODO */ }) {
                     Icon(
-                        imageVector = Icons.Filled.Send,
+                        imageVector = Icons.Filled.Share,
                         contentDescription = "共有",
-                        modifier = modifier
-                            .weight(1f)
-                            .width(20.dp),
+                        modifier = Modifier.width(20.dp),
                         tint = shareButtonColor
                     )
                 }
@@ -93,13 +112,16 @@ fun ListCard(index: Int, cardItem: CardItem, modifier: Modifier = Modifier) {
                     Icon(
                         imageVector = Icons.Filled.MoreVert,
                         contentDescription = "その他",
-                        modifier = modifier
-                            .weight(1f)
-                            .width(20.dp),
+                        modifier = Modifier.width(20.dp),
                     )
                 }
             }
-            Divider(color = Color.Gray)
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewListCard() {
+    ListCard(index = 1, cardItem = CardItem("Task 1", "数値計算 WebClass", "text","2023/8/20/17:00",  false,true))
 }

45144bc3312de5320bf51accb9f2b4ed067928c7
kototo522 <huyejiben@gmail.com>
2023-12-19 14:09:17 +0900

    add: Hiltの導入

---
+++ app/src/main/java/com/example/kosenstride/KosenStrideApplication.kt
@@ -0,0 +1,7 @@
+package com.example.kosenstride
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KosenStrideApplication : Application()
--- app/src/main/java/com/example/kosenstride/MainActivity.kt
+++ app/src/main/java/com/example/kosenstride/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.kosenstride.navigation.BottomNavigationBar
 import com.example.kosenstride.ui.theme.KosenStrideTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

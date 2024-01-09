6513091786d221a1035e58fe3bc303498fd54494
kototo522 <huyejiben@gmail.com>
2024-01-09 14:22:43 +0900

    add: timePicker&datePickerの追加

--- app/src/main/java/com/example/kosenstride/ui/createTodo/CreateTodoScreen.kt
+++ app/src/main/java/com/example/kosenstride/ui/createTodo/CreateTodoScreen.kt
@@ -1,5 +1,8 @@
 package com.example.kosenstride.ui.createTodo
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,12 +14,17 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,20 +37,38 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.kosenstride.navigation.BottomBarScreen
+import com.example.kosenstride.ui.createTodo.component.ChangeDateFormat
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.util.Date
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateTodoScreen(navController: NavController) {
+    val datePickerState =
+        rememberDatePickerState(
+            initialSelectedDateMillis = Instant.now().toEpochMilli(),
+        )
+    val timePickerState = rememberTimePickerState()
     var addTitleText by remember { mutableStateOf("") }
     var addText by remember { mutableStateOf("") }
-    var dateTimeText by remember { mutableStateOf("") }
+    val dateText =
+        remember {
+            mutableStateOf(
+                ChangeDateFormat(Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant())),
+            )
+        }
+    val timeText = remember { mutableStateOf("23:59") }
     val checkedState = remember { mutableStateOf(false) }
+    var datePickerExpended by remember { mutableStateOf(false) }
+    var timePickerExpended by remember { mutableStateOf(false) }
 
     val createItems =
         listOf(
             AddItem("タイトル", addTitleText) { addTitleText = it },
             AddItem("内容", addText) { addText = it },
-            AddItem("期限", dateTimeText) { dateTimeText = it },
         )
 
     Column {
@@ -84,6 +110,27 @@ fun CreateTodoScreen(navController: NavController) {
             Spacer(modifier = Modifier.height(24.dp))
         }
 
+        Text(text = "期限", fontSize = 14.sp, modifier = Modifier.padding(horizontal = 20.dp))
+        Row(modifier = Modifier.padding(vertical = 8.dp)) {
+            Text(
+                text = dateText.value,
+                fontSize = 16.sp,
+                modifier =
+                    Modifier
+                        .padding(horizontal = 20.dp)
+                        .clickable { datePickerExpended = true },
+            )
+            Text(
+                text = timeText.value,
+                fontSize = 16.sp,
+                modifier =
+                    Modifier
+                        .padding(horizontal = 20.dp)
+                        .clickable { timePickerExpended = true },
+            )
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+
         Row(
             modifier =
                 Modifier
@@ -96,6 +143,47 @@ fun CreateTodoScreen(navController: NavController) {
                 onCheckedChange = { checkedState.value = it },
                 modifier = Modifier.height(14.dp),
             )
+        }
+    }
+    if (datePickerExpended) {
+        DatePickerDialog(
+            onDismissRequest = { datePickerExpended = false },
+            dismissButton = {
+                Text(text = "キャンセル", modifier = Modifier.padding(10.dp).clickable { datePickerExpended = false })
+            },
+            confirmButton = {
+                Text(
+                    text = "OK",
+                    modifier =
+                        Modifier.padding(10.dp).clickable {
+                            val selectedDateMillis = datePickerState.selectedDateMillis
+                            dateText.value = ChangeDateFormat(Date(selectedDateMillis!!))
+                            datePickerExpended = false
+                        },
+                )
+            },
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+    if (timePickerExpended) {
+        DatePickerDialog(
+            onDismissRequest = { timePickerExpended = false },
+            dismissButton = {
+                Text(text = "キャンセル", modifier = Modifier.padding(10.dp).clickable { timePickerExpended = false })
+            },
+            confirmButton = {
+                Text(
+                    text = "OK",
+                    modifier =
+                        Modifier.padding(10.dp).clickable {
+                            timeText.value = "${timePickerState.hour}:${timePickerState.minute}"
+                            timePickerExpended = false
+                        },
+                )
+            },
+        ) {
+            TimePicker(state = timePickerState, modifier = Modifier.align(Alignment.CenterHorizontally))
         }
     }
 }

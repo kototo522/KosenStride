3b621418d9707fcd50b54d79fccf0c83c181d06b
kototo522 <huyejiben@gmail.com>
2024-01-16 11:52:03 +0900

    delete: コメントの削除

--- app/src/main/java/com/example/kosenstride/MainActivity.kt
+++ app/src/main/java/com/example/kosenstride/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxWidth(),
                 ) {
                     Column {
-//                        KosenStrideTopAppBar()
                         Navigation()
                     }
                 }

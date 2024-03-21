f038fedfc899c566130cea7d36869da46a0beffc
kototo522 <huyejiben@gmail.com>
2024-03-21 21:40:15 +0900

    update setting list

--- app/src/main/java/com/example/kosenstride/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/example/kosenstride/ui/setting/SettingScreen.kt
@@ -1,6 +1,5 @@
 package com.example.kosenstride.ui.setting
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,13 +10,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kosenstride.ui.setting.component.SettingCard
 
-data class SettingItem(val title: String, val text: String, val onClick: () -> Unit)
+data class SettingItem(val title: String, val onClick: () -> Unit)
 
 @Composable
 fun SettingScreen() {
     val settingsList = listOf(
-        SettingItem("General", "Configure general settings") { /* Handle click */ },
-        SettingItem("Notifications", "Configure notification settings") { /* Handle click */ },
+        SettingItem("プロフィール編集") { /* Handle click */ },
+        SettingItem("プロフィール編集") { /* Handle click */ },
     )
     LazyColumn(
         modifier = Modifier
@@ -29,7 +28,6 @@ fun SettingScreen() {
             val setting = settingsList[index]
             SettingCard(
                 title = setting.title,
-                text = setting.text,
                 onItemClick = setting.onClick)
         }
     }
--- app/src/main/java/com/example/kosenstride/ui/setting/component/SettingCard.kt
+++ app/src/main/java/com/example/kosenstride/ui/setting/component/SettingCard.kt
@@ -2,12 +2,15 @@ package com.example.kosenstride.ui.setting.component
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
@@ -16,13 +19,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.kosenstride.R
 
 @Composable
-fun SettingCard(title: String, text: String, onItemClick: () -> Unit) {
+fun SettingCard(title: String, onItemClick: () -> Unit) {
     val borderColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.2f)
 
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp, horizontal = 12.dp)
             .clickable { onItemClick() }
             .drawBehind {
                 drawLine(
@@ -37,12 +39,7 @@ fun SettingCard(title: String, text: String, onItemClick: () -> Unit) {
             text = title,
             style = MaterialTheme.typography.titleMedium,
             color = MaterialTheme.colorScheme.onSurface,
-        )
-        Text(
-            text = text,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSurface,
-            modifier = Modifier.padding(vertical = 8.dp),
+            modifier = Modifier.padding(vertical = 20.dp, horizontal = 16.dp),
         )
     }
 }
@@ -50,5 +47,5 @@ fun SettingCard(title: String, text: String, onItemClick: () -> Unit) {
 @Preview
 @Composable
 fun PreviewSettingCard() {
-    SettingCard(title = "title", text = "hogehogehoge", onItemClick = {},)
+    SettingCard(title = "title", onItemClick = {},)
 }

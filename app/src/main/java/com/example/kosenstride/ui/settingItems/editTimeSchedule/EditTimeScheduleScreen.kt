46ff88fa15e3014e2ceb5e6fd8cae56e84ea0ae0
kototo522 <huyejiben@gmail.com>
2024-03-29 23:49:00 +0900

    delete: shadow

--- app/src/main/java/com/example/kosenstride/ui/settingItems/editTimeSchedule/EditTimeScheduleScreen.kt
+++ app/src/main/java/com/example/kosenstride/ui/settingItems/editTimeSchedule/EditTimeScheduleScreen.kt
@@ -2,35 +2,25 @@ package com.example.kosenstride.ui.settingItems.editTimeSchedule
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import com.example.kosenstride.ui.home.Class
 import com.example.kosenstride.ui.settingItems.editTimeSchedule.component.ClassCard
 import org.burnoutcrew.reorderable.ReorderableItem
 import org.burnoutcrew.reorderable.detectReorder
 import org.burnoutcrew.reorderable.detectReorderAfterLongPress
 import org.burnoutcrew.reorderable.rememberReorderableLazyListState
 import org.burnoutcrew.reorderable.reorderable
-import java.nio.file.Files.move
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
@@ -66,14 +56,12 @@ fun EditTimeScheduleScreen() {
             ReorderableItem(
                 reorderableState = state,
                 key = item,
-            ) { isDragging ->
-                val elevation = animateDpAsState(if (isDragging) 8.dp else 0.dp, label = "")
+            ) {
                 Column(
                     modifier = Modifier
-                        .shadow(elevation.value)
                         .fillMaxWidth()
                         .detectReorder(state)
-                        .clickable {  }
+                        .clickable {}
                         .padding(horizontal = 16.dp, vertical = 8.dp)
                 ) {
                     ClassCard(
